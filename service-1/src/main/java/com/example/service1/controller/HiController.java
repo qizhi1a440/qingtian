bd128a66fc76001e6ad183b7690d8e19620d0547
wangjianguo <qizhi1a440@outlook.com>
2019-07-01 11:15:52 +0800

    分布式事务 高可用 使用redis哨兵实现

--- service-1/src/main/java/com/example/service1/controller/HiController.java
+++ service-1/src/main/java/com/example/service1/controller/HiController.java
@@ -2,12 +2,18 @@ package com.example.service1.controller;
 
 import com.example.service1.service.HiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HiController {
+
+    @Value("${server.port}")
+    private String port;
 
     @Autowired
     private HiService hiService;
@@ -15,5 +21,10 @@ public class HiController {
     @RequestMapping("/hi")
     public String hi(@RequestParam String name){
         return hiService.hi(name);
+    }
+
+    @RequestMapping("/port")
+    public String port(){
+        return port;
     }
 }
